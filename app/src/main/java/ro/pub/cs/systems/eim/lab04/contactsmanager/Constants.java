189aac46a474c15e2543ff0844e6bea0d7eb500f
Student User <student@cti.pub.ro>
2018-03-16 09:56:42 +0200

    lab 4 tasks

---
+++ app/src/main/java/ro/pub/cs/systems/eim/lab04/contactsmanager/Constants.java
@@ -0,0 +1,9 @@
+package ro.pub.cs.systems.eim.lab04.contactsmanager;
+
+/**
+ * Created by student on 16.03.2018.
+ */
+
+public interface Constants {
+    final public static int CONTACTS_MANAGER_REQUEST_CODE = 2018;
+}
